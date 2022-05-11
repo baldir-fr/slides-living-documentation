d8c1d35db55dd69dfd91eef31d34c73f13aa16fd
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-05-11 23:06:49 +0200

    example: doctest elixir

---
+++ examples/doctest_elixir/lib/num.ex
@@ -0,0 +1,14 @@
+defmodule Num do
+    @moduledoc """
+    Operations on numbers
+    """
+    @doc """
+    Adds 2 numbers
+    ## Examples
+        iex> Num.sum(2, 2)
+        5
+    """
+    def sum(a, b) do
+        a + b
+    end
+end
---
+++ examples/doctest_elixir/test/num_test.exs
@@ -0,0 +1,4 @@
+defmodule NumTest do
+  use ExUnit.Case
+  doctest Num
+end

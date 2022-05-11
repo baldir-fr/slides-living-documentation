5363d2da46d9f8ac630e90daa8c4f9fc570de9fc
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-05-12 00:23:07 +0200

    examples: rust documentation tests

---
+++ examples/documentation_test_rust/src/lib.rs
@@ -0,0 +1,9 @@
+/// Adds two numbers together:
+/// ```
+/// # use documentation_test_rust::add;
+/// let c = add(2, 2);
+/// # assert_eq!(c, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+  return a + b;
+}
